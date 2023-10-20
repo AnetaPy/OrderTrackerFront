767b7070ab94dbf2a0c05d825d6aca232a1e3d8e
AnetaPy <aneta.pych15@gmail.com>
2023-10-20 22:06:21 +0200

    Added login page, images and variables in CSS.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,70 @@
-import './page.css';
+import './page.css'
+
 const Login = () => {
     return (
-        <div className="main">
-            <h1>Hi</h1>
-            <h2>User</h2>
+        <div className="login">
+            <div className="login_introduction">
+                <img src="/images/login_img.jpg" alt="People working at computers with charts."/>
+                <h1>Witaj w OrderTracker</h1>
+                <p>Wszystkie zamówienia, materiały i progress w jednym miejscu</p>
+            </div>
+
+            <div className="login_container">
+                <img src="/images/logo.png" alt=""/>
+                <h2>Logowanie</h2>
+                <div className="login_portals">
+                    <button>
+                        <div className="login_button">
+                            <img src="/images/icon_google.png" alt=""/>
+                            <p>Zaloguj się z Google</p>
+                        </div>
+                    </button>
+                    <button>
+                        <div className="login_button">
+                            <img src="/images/icon_github.png" alt=""/>
+                            <p>Zaloguj się z GitHub</p>
+                        </div>
+                    </button>
+                </div>
+                <div className="login_divider">
+                    <div className="login_line"></div>
+                    <p>lub</p>
+                    <div className="login_line"></div>
+                </div>
+                <div className="login_form">
+                    <form>
+                        <label>
+                            E-mail
+                            <input
+                                type="email"
+                                // value={email}
+                                // onChange={(e) => setEmail(e.target.value)}
+                                className="login_email"
+                                placeholder="E-mail"
+                            />
+                        </label>
+                        <label>
+                            Hasło
+                            <input
+                                type="password"
+                                // value={password}
+                                // onChange={(e) => setPassword(e.target.value)}
+                                className="login_password"
+                                placeholder="Hasło"
+                            />
+                        </label>
+                        <button
+                            // disabled={loading || !email || !password}
+                        > Zaloguj
+                            {/*{loading ? "Please wait.." : "Submit"}*/}
+                        </button>
+                    </form>
+                    <a href="/signup">
+                        <p>Nie masz konta? <span>Zarejestruj się!</span></p>
+                    </a>
+                </div>
+
+            </div>
         </div>
     )
 }
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+const Register = () => {
+    return (
+        <div>
+            <h1 style={{color: "#000"}}>Rejestracja</h1>
+        </div>
+    )
+}
+
+export default Register;
