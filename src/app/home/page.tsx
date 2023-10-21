eb028b9baf7206fec4dd9a5d8ecf950c4fbe81e9
AnetaPy <aneta.pych15@gmail.com>
2023-10-21 20:35:37 +0200

    Created a simple home page structure.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,30 @@
+import './page.css';
+import 'react-calendar/dist/Calendar.css';
+import {MyCalendar} from "@/components/MyCalendar/MyCalendar";
 
-const Ad = () => {
+
+const Home = () => {
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="Home">
+            <div className="Home_navigation"></div>
+            <div className="Home_container">
+                <div className="Home_header">
+                    <h1>Witaj Aneta!</h1>
+                    <h2>Od czego dzisiaj zaczynamy?</h2>
+                </div>
+                <div className="Home_wrapper">
+                    <div className="Home_calendar">
+                        {/*<Calendar/>*/}
+                        <MyCalendar/>
+                    </div>
+                    <div className="Home_actual_order"></div>
+                    <div className="Home_add_material"></div>
+                    <div className="Home_add_order"></div>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Ad;
+export default Home;
