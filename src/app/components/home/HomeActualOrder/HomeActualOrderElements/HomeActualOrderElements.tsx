4fcfc7d75a21a392fa8eac7032767c6a927e7bc2
AnetaPy <aneta.pych15@gmail.com>
2023-10-27 13:29:07 +0200

    Changed the CSS file in the "Actual Order" section on the home page.

--- src/app/components/home/HomeActualOrder/HomeActualOrderElements/HomeActualOrderElements.tsx
+++ src/app/components/home/HomeActualOrder/HomeActualOrderElements/HomeActualOrderElements.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './HomeActualOrderElements.css';
 
 export const HomeActualOrderElements = () => {
     return (
--- src/app/components/home/HomeActualOrder/HomeActualOrderMaterialsUsed/HomeActualOrderMaterialsUsed.tsx
+++ src/app/components/home/HomeActualOrder/HomeActualOrderMaterialsUsed/HomeActualOrderMaterialsUsed.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './HomeActualOrderMaterialsUsed.css';
 
 export const HomeActualOrderMaterialsUsed = () => {
     return (
