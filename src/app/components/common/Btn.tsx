8824d47b724831eaa385fa63035aa59b27ce38b8
AnetaPy <aneta.pych15@gmail.com>
2023-10-29 11:42:30 +0100

    Updated Btn component.

--- src/app/components/common/Btn.tsx
+++ src/app/components/common/Btn.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import {Icon} from "@/components/common/Icon";
 import './Btn.css';
+
 interface Props {
-    text:string;
+    nameOfClass?: string;
+    text: string;
     src?: string;
-    alt?:string;
+    alt?: string;
 }
+
 export const Btn = (props: Props) => {
-    return <button>
-        {props.src ? <Icon src={props.src} alt={props.alt} /> : null}
+    return <button className={props.nameOfClass}>
+        {props.src ? <Icon src={props.src} alt={props.alt}/> : null}
         {props.text}
     </button>
 }
