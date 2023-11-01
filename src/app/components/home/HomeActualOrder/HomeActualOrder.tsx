ee2b6bd54ff07f7c14f7e3b97991601080ee9637
AnetaPy <aneta.pych15@gmail.com>
2023-11-01 06:31:53 +0100

    Information about one actual order was downloaded from the database.

--- src/app/components/home/HomeActualOrder/HomeActualOrder.tsx
+++ src/app/components/home/HomeActualOrder/HomeActualOrder.tsx
@@ -1,25 +1,58 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './HomeActualOrder.css';
-import {OrderInfoMaterialsUsed} from "@/components/common/OrderInfo/OrderInfoMaterialsUsed/OrderInfoMaterialsUsed";
-import {OrderInfoElementsUsed} from "@/components/common/OrderInfo/OrderInfoElementsUsed/OrderInfoElementsUsed";
+import {OrderInfoMaterialsUsed} from "@/components/common/OrderInfo/OrderInfoMaterialsUsed";
+import {OrderInfoElementsUsed} from "@/components/common/OrderInfo/OrderInfoElementsUsed";
+import {Spinner} from "@/components/common/Spinner/Spinner";
+import {OrderEntity} from "types";
 
-export const HomeActualOrder = () => {
+interface Props {
+    id: string;
+}
+
+export const HomeActualOrder = (props: Props) => {
+    const [order, setOrder] = useState<OrderEntity | null>(null);
+    let difference;
+    useEffect(() => {
+        (async () => {
+            const res = await fetch(`http://localhost:3001/home/${props.id}`);
+            const data = await res.json();
+            setOrder(data);
+        })()
+    }, []);
+    if (order === null) {
+        return (
+            <div className="Home_actual_order">
+                <div className="Home_actual_order_container">
+                    <Spinner/>
+                </div>
+            </div>)
+    }
+
+    if (order?.date) {
+        const currentDate = new Date().getTime();
+        const deadlineDate = new Date(order.date).getTime();
+        difference = Math.ceil((deadlineDate - currentDate) / (1000 * 3600 * 24));
+    }
+
     return (
         <div className="Home_actual_order">
             <div className="Home_actual_order_container">
-                <h2>Zam 1</h2>
-                <p>W trakcie</p>
-                <p className="Home_notification_orange">Pozostało Ci 8 dni do końca terminu</p>
+                <h2>{order?.name}</h2>
+                <p>{order?.status}</p>
+                {difference > 0
+                    ? <p className={difference > 7 ? "Home_notification_green" : "Home_notification_orange"}>Pozostało
+                        Ci {difference} dni do końca terminu</p>
+                    : <p className="Home_notification_red">Jesteś {-difference} dni po terminie</p>}
                 <div className="Home_actual_order_container_items">
                     <div className="Home_deadline">
-                        <h3>Deadline</h3>
+                        <h3>Termin</h3>
                         <div className="Home_deadline_container">
-                            <h4>10 listopad 2023</h4>
-                            <p>Do końca pozostało 22 dni</p>
+                            <h4>{order.date ? new Date(order.date).toLocaleDateString() : null}</h4>
+                            <p>Do końca pozostało {difference} dni</p>
                         </div>
                     </div>
-                    <OrderInfoElementsUsed/>
-                    <OrderInfoMaterialsUsed text={"Wykorzystane materiały"}/>
+                    <OrderInfoElementsUsed elements={order.elements} text="Elementy"/>
+                    <OrderInfoMaterialsUsed materials={order.materials} text="Wykorzystane materiały"/>
                 </div>
             </div>
         </div>
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,14 @@
+"use client"
+
 import React from "react";
 import './page.css';
 import 'react-calendar/dist/Calendar.css';
-import {Navigation} from "@/components/common/Navigation";
+import {Navigation} from "@/components/common/Navigation/Navigation";
 import {HomeCalendar} from "@/components/home/HomeCalendar/HomeCalendar";
 import {HomeActualOrder} from "@/components/home/HomeActualOrder/HomeActualOrder";
 import {AddMaterial} from "@/components/common/Add/AddMaterial/AddMaterial";
 import {AddOrder} from "@/components/common/Add/AddOrder/AddOrder";
-import {Header} from "@/components/common/Header";
+import {Header} from "@/components/common/Header/Header";
 
 const Home = () => {
 
@@ -17,7 +19,7 @@ const Home = () => {
                 <Header text="Witaj Aneta!"/>
                 <div className="Home_wrapper">
                     <HomeCalendar/>
-                    <HomeActualOrder/>
+                    <HomeActualOrder id="13d02494-77cf-11ee-9ece-34cff6159130"/>
                     <AddMaterial/>
                     <AddOrder/>
                 </div>
