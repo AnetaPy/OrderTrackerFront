562cfe51ba11eb099cbfd1881162ea24eb8bedae
AnetaPy <aneta.pych15@gmail.com>
2023-11-10 19:59:46 +0100

    Added signupForm handler.

--- src/app/components/signup/SignupForm/SignupForm.tsx
+++ src/app/components/signup/SignupForm/SignupForm.tsx
@@ -1,19 +1,82 @@
-import React from "react";
+'use client'
+
+import React, {SyntheticEvent, useState} from "react";
 import './SignupForm.css';
+import {Spinner} from "@/components/common/Spinner/Spinner";
+import {redirect} from "next/navigation";
+import Link from "next/link";
 
 export const SignupForm = () => {
+    const [loading, setLoading] = useState(false);
+    const [username, setUsername] = useState(null);
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        passwordFirst: '',
+        passwordSecond: ''
+    });
+    const {email, passwordFirst, name, passwordSecond} = form;
+
+    const saveUser = async (e: SyntheticEvent) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            const res = await fetch(`http://localhost:3001/user/signup`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(form)
+            });
+            if (res.status === 400 || res.status === 500) {
+                const err = await res.json();
+                alert(`Błąd: ${err.message}`)
+                return;
+            }
+            const data = await res.json();
+            setUsername(data.name)
+        } finally {
+            setLoading(false);
+        }
+
+    }
+    if (loading) {
+        return <Spinner/>
+
+    }
+    if (username) {
+        redirect("http://localhost:3000")
+    }
+
+    const updateForm = (key: string, value: any) => {
+        setForm(form => ({
+            ...form,
+            [key]: value,
+        }))
+
+    }
     return (
         <div className="Signup_container">
-            <img src="/images/logo.png" alt=""/>
+            <Link href="/home"><img src="/images/logo.png" alt="Logo OrderTracker"/></Link>
             <h2>Rejestracja</h2>
             <div className="Signup_form">
-                <form>
+                <form onSubmit={saveUser}>
+                    <label>
+                        Imię
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={e => updateForm('name', e.target.value)}
+                            className="Login_name"
+                            placeholder="Imię"
+                        />
+                    </label>
                     <label>
                         E-mail
                         <input
                             type="email"
-                            // value={email}
-                            // onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                            onChange={e => updateForm('email', e.target.value)}
                             className="Login_email"
                             placeholder="E-mail"
                         />
@@ -22,8 +85,8 @@ export const SignupForm = () => {
                         Hasło
                         <input
                             type="password"
-                            // value={password}
-                            // onChange={(e) => setPassword(e.target.value)}
+                            value={passwordFirst}
+                            onChange={e => updateForm('passwordFirst', e.target.value)}
                             className="Login_password"
                             placeholder="Hasło"
                         />
@@ -32,16 +95,15 @@ export const SignupForm = () => {
                         Powtórz hasło
                         <input
                             type="password"
-                            // value={password}
-                            // onChange={(e) => setPassword(e.target.value)}
+                            value={passwordSecond}
+                            onChange={e => updateForm('passwordSecond', e.target.value)}
                             className="Login_password"
                             placeholder="Powtórz hasło"
                         />
                     </label>
                     <button
-                        // disabled={loading || !email || !password}
+                        disabled={!email || !passwordFirst || !passwordSecond}
                     > Zarejestruj się
-                        {/*{loading ? "Please wait.." : "Submit"}*/}
                     </button>
                 </form>
             </div>
