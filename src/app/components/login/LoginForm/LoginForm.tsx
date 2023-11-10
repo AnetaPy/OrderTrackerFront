03bc5a4bea0afe1abf779f6b2148d1fa8e98e49e
AnetaPy <aneta.pych15@gmail.com>
2023-11-10 19:57:01 +0100

    Added user login with bcrypt.

--- src/app/components/login/LoginForm/LoginForm.tsx
+++ src/app/components/login/LoginForm/LoginForm.tsx
@@ -1,36 +1,60 @@
-import React from "react";
+"use client"
+
+import React, {FormEvent, useRef, useState} from "react";
 import './LoginForm.css';
 import Link from "next/link";
+import {redirect} from "next/navigation";
 
 export const LoginForm = () => {
-    return(
+    const [loading, setLoading] = useState(false);
+    const email = useRef("");
+    const password = useRef("");
+    const onSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        const res = await fetch(`http://localhost:3001/user/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
+        const data = await res.json();
+        if (data) {
+            setLoading(true)
+        } else {
+            alert("Błędny login lub hasło!");
+        }
+    }
+
+    if (loading){
+        redirect('/home')
+    }
+
+    return (
         <div className="Login_form">
-            <form>
+            <form onSubmit={onSubmit}>
                 <label>
                     E-mail
                     <input
-                        type="email"
-                        // value={email}
-                        // onChange={(e) => setEmail(e.target.value)}
+                        type="e-mail"
+                        onChange={(e) => (email.current = e.target.value)}
                         className="Login_email"
-                        placeholder="E-mail"
+                        placeholder="Email"
                     />
                 </label>
                 <label>
                     Hasło
                     <input
                         type="password"
-                        // value={password}
-                        // onChange={(e) => setPassword(e.target.value)}
+                        onChange={(e) => (password.current = e.target.value)}
                         className="Login_password"
                         placeholder="Hasło"
                     />
                 </label>
-                <button
-                    // disabled={loading || !email || !password}
-                > Zaloguj
-                    {/*{loading ? "Please wait.." : "Submit"}*/}
-                </button>
+                <button onClick={onSubmit}>Zaloguj</button>
             </form>
             <Link href="/signup"><p>Nie masz konta? <span>Zarejestruj się!</span></p></Link>
         </div>
