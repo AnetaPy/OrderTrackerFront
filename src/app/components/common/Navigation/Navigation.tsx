85ab4416b7d4a383ad25c86c7f273bebe3d3a515
AnetaPy <aneta.pych15@gmail.com>
2023-11-10 19:55:48 +0100

    Updated navigation.

--- src/app/components/common/Navigation/Navigation.tsx
+++ src/app/components/common/Navigation/Navigation.tsx
@@ -2,14 +2,10 @@ import React from "react";
 import './Navigation.css';
 import Link from "next/link";
 
-export const Navigation = () => {
+export const Navigation = (props) => {
     return (
         <div className="Navigation">
             <div className="Navigation_user_details">
-                <img src="/icons/user.png" alt="User image"/>
-                <div className="Navigation_user_information">
-                    <h3>Aneta</h3>
-                    <p>aneta@gmail.com</p></div>
             </div>
             <nav>
                 <ul>
@@ -21,10 +17,8 @@ export const Navigation = () => {
                             <p>Materiały</p></Link></li>
                     </div>
                     <div>
-                        {/*<li><Link href="/contact"><img src="/icons/contact.png" alt="Envelope image"/><p>Kontakt</p>*/}
-                        {/*</Link></li>*/}
-                        {/*<li><Link href="/log-out"><img src="/icons/log_out.png" alt="Arrow right image"/><p>Wyloguj*/}
-                            {/*się</p></Link></li>*/}
+                        <li><Link href="/"><img src="/icons/log_out.png" alt="Arrow right image"/>
+                            <p>Wyloguj się</p></Link></li>
                     </div>
                 </ul>
             </nav>
